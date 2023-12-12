1e1cd052490064f03c383f759204eb51be5d91b4
Trung Hoang <trunghq3101@gmail.com>
2023-12-13 06:58:56 +0700

    improve UI for checkbox

--- lib/features/todo_list/todo_list_view.dart
+++ lib/features/todo_list/todo_list_view.dart
@@ -18,41 +18,52 @@ class _TodoListViewState extends State<TodoListView> {
         return ListView(
           children: [
             ...value.todos.map(
-              (todo) => Container(
-                margin: const EdgeInsets.only(top: 12),
-                padding: const EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFFFFFF),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Row(
-                  children: [
-                    Checkbox(
-                      value: todo.isDone,
-                      onChanged: (isDone) {
-                        value.toggleIsDone(todo.id);
-                      },
-                    ),
-                    Expanded(
-                      child: Text(
-                        todo.title,
-                        style: const TextStyle(
-                          color: Color(0xFF000000),
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+              (todo) => GestureDetector(
+                onTap: () {
+                  value.toggleIsDone(todo.id);
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(top: 12),
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFFFFFF),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 12),
+                        child: todo.isDone
+                            ? const Icon(
+                                Icons.check_box,
+                                color: Color.fromARGB(255, 73, 78, 175),
+                              )
+                            : const Icon(
+                                Icons.check_box_outline_blank,
+                                color: Color.fromARGB(255, 73, 78, 175),
+                              ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          todo.title,
+                          style: const TextStyle(
+                            color: Color(0xFF000000),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        value.remove(todo.id);
-                      },
-                      child: const Icon(
-                        Icons.delete,
-                        color: Color(0xFFAF4949),
+                      GestureDetector(
+                        onTap: () {
+                          value.remove(todo.id);
+                        },
+                        child: const Icon(
+                          Icons.delete,
+                          color: Color(0xFFAF4949),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
