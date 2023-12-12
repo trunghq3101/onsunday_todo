ef0044ab5f633602adac3ead0227ed36ce3f463e
Trung Hoang <trunghq3101@gmail.com>
2023-12-12 22:39:27 +0700

    add Directionality

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFFAAAFFF),
-      padding: const EdgeInsets.all(24),
-      child: Column(
-        children: [
-          const Text(
-            'To-do List app',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              color: Color(0xFF000000),
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              margin: const EdgeInsets.only(top: 24),
-              padding: const EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                color: const Color(0xFF0000FF),
-                borderRadius: BorderRadius.circular(12),
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        color: const Color(0xFFAAAFFF),
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            const Text(
+              'To-do List app',
+              style: TextStyle(
+                color: Color(0xFF000000),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              child: const Text(
-                'Add new todo',
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                  color: Color(0xFFFFFFFF),
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: const EdgeInsets.only(top: 24),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF0000FF),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Text(
+                  'Add new todo',
+                  style: TextStyle(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
