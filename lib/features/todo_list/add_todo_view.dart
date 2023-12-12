8d477fc5998c0a6ce838a4acd5ea6028c7848b9a
Trung Hoang <trunghq3101@gmail.com>
2023-12-13 06:19:51 +0700

    raw EditableText will not work without lots of effort

--- lib/features/todo_list/add_todo_view.dart
+++ lib/features/todo_list/add_todo_view.dart
@@ -1,17 +1,71 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:provider/provider.dart';
 
-import 'todo_list_model.dart';
-
-class AddTodoView extends StatelessWidget {
+class AddTodoView extends StatefulWidget {
   const AddTodoView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<AddTodoView> createState() => _AddTodoViewState();
+}
+
+class _AddTodoViewState extends State<AddTodoView> {
+  bool _isEditing = false;
+  late final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  Widget _buildEditingView() {
+    return Container(
+      margin: const EdgeInsets.only(top: 24),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFFFFF),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: EditableText(
+              focusNode: _focusNode,
+              controller: _controller,
+              autofocus: true,
+              style: const TextStyle(
+                color: Color(0xFF000000),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              cursorColor: const Color(0xFF000000),
+              backgroundCursorColor: const Color(0xFF000000),
+              onChanged: (value) {},
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _isEditing = false;
+              });
+            },
+            child: const Text(
+              'Done',
+              style: TextStyle(
+                color: Color(0xFF0000FF),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAddButton() {
     return GestureDetector(
       onTap: () {
-        final model = Provider.of<TodoListModel>(context, listen: false);
-        model.addTodo('New todo');
+        setState(() {
+          _isEditing = true;
+        });
+        // final model = Provider.of<TodoListModel>(context, listen: false);
+        // model.addTodo('New todo');
       },
       child: Container(
         margin: const EdgeInsets.only(top: 24),
@@ -30,5 +84,14 @@ class AddTodoView extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isEditing) {
+      return _buildEditingView();
+    } else {
+      return _buildAddButton();
+    }
   }
 }
