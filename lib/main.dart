be8390b5d99af88744815d9f239c86e69520f7e3
Trung Hoang <trunghq3101@gmail.com>
2023-12-13 06:25:54 +0700

    use MaterialApp and Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:onsunday_todo/features/todo_list/add_todo_view.dart';
 import 'package:onsunday_todo/features/todo_list/todo_list_view.dart';
 import 'package:provider/provider.dart';
@@ -16,26 +16,27 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => TodoListModel(),
-      child: Directionality(
-        textDirection: TextDirection.ltr,
-        child: Container(
-          color: const Color(0xFFAAAFFF),
-          padding: const EdgeInsets.all(24),
-          child: const Column(
-            children: [
-              Text(
-                'To-do List app',
-                style: TextStyle(
-                  color: Color(0xFF000000),
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      child: MaterialApp(
+        home: Scaffold(
+          body: Container(
+            color: const Color(0xFFAAAFFF),
+            padding: const EdgeInsets.all(24),
+            child: const Column(
+              children: [
+                Text(
+                  'To-do List app',
+                  style: TextStyle(
+                    color: Color(0xFF000000),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Expanded(
-                child: TodoListView(),
-              ),
-              AddTodoView(),
-            ],
+                Expanded(
+                  child: TodoListView(),
+                ),
+                AddTodoView(),
+              ],
+            ),
           ),
         ),
       ),
