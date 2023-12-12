9aa116d51ab76c95d0aae43c93859525708717a1
Trung Hoang <trunghq3101@gmail.com>
2023-12-12 22:22:26 +0700

    show <<add new todo>> button

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,38 @@ class MainApp extends StatelessWidget {
     return Container(
       color: const Color(0xFFAAAFFF),
       padding: const EdgeInsets.all(24),
-      child: const Text(
-        'To-do List app',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          color: Color(0xFF000000),
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
+      child: Column(
+        children: [
+          const Text(
+            'To-do List app',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Color(0xFF000000),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.only(top: 24),
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: const Color(0xFF0000FF),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Text(
+                'Add new todo',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  color: Color(0xFFFFFFFF),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
